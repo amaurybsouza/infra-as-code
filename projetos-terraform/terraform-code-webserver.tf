b133a416a3b5919aed67cdc5b2c1a5d401bfe9a1
amaurybsouza <amaurybsouza@gmail.com>
2019-12-09 09:13:49 -0300

    new code

--- projetos-terraform/terraform-code-webserver.tf
+++ projetos-terraform/terraform-code-webserver.tf
@@ -5,3 +5,17 @@ provider "docker" {
 resource "docker_container" "httpd" {
   
 }
+
+resource "dockerimage" "apache" {
+  
+}
+
+resource "type" "name" {
+  
+}
+
+resource "docker_container" "apache2" {
+    name = "Amaury"
+  
+}
+
